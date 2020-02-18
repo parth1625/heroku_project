b4c26deebffaea809ee2037c3eac8f17902ef3cd
Parth Vijay <parth@securitytroops.com>
2020-02-18 18:22:53 +0530

    added static urlpatterns in urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('polls/', include('polls.urls')),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
 
