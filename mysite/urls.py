62169bf26fce6de1365d4eb572f2cc16c99a4d28
Parth Vijay <parth@securitytroops.com>
2020-02-18 18:39:49 +0530

    changes in url.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('polls/', include('polls.urls')),    
 ]
 
-urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
