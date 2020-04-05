7d4e4e3416e2a0c6bff873b9e473e8733479c4ba
Parth Vijay <parthvj1998@gmail.com>
2020-04-05 11:53:56 +0530

    changes in test.py

--- polls/tests.py
+++ polls/tests.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 from .models import Question
 from django.urls import reverse
 
-
 class QuestionModelTests(TestCase):
 
 	def test_was_published_recently_with_future_question(self):
